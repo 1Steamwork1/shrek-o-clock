9239a839e4e130c9e6d6dc2077f89aa645db4b42
Valentin Stoykov <v.s.warwick.cs@gmail.com>
2025-01-02 11:12:41 +0200

    ref: Updated file paths for image and audio imports

--- src/data.ts
+++ src/data.ts
@@ -8,20 +8,20 @@ export type GiftData = {
 export const data: GiftData[] = [
   {
     date: "01 Jan 2025",
-    imageUrl: "/src/media/donkey.png",
-    audioUrl: "/src/media/donkey.mp3",
+    imageUrl: "../media/donkey.png",
+    audioUrl: "../media/donkey.mp3",
     quote: "Don't be an onion, shrek!",
   },
   {
     date: "02 Jan 2025",
-    imageUrl: "/src/media/puss.png",
-    audioUrl: "/src/media/puss.mp3",
+    imageUrl: "../media/puss.png",
+    audioUrl: "../media/puss.mp3",
     quote: "I owe you my life sir!",
   },
   {
     date: "03 Jan 2025",
-    imageUrl: "/src/media/ogre.png",
-    audioUrl: "/src/media/ogre.mp3",
+    imageUrl: "../media/ogre.png",
+    audioUrl: "../media/ogre.mp3",
     quote: "I am head ogre heels for you.",
   },
 ];
